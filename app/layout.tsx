7c532963790bea10602898dd71379f2909710147
nyebe-14 <ramirezian037@gmail.com>
2023-08-13 00:40:43 +0800

    Add Database Connection
    - Database Provided By PlanetScale
    - Managed By Prisma
    - Use React Toast for Information Response
    - Create Prisma Table Model (Store)
    - Create API (Stores - Post)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToastProvider } from '@/providers/toast-provider'
 
 const inter = Inter( { subsets: [ 'latin' ] } )
 
@@ -26,6 +27,7 @@ export default function RootLayout ( {
         <body className={ inter.className }>
           <main>
             <ModalProvider />
+            <ToastProvider />
             { children }
           </main>
         </body>
